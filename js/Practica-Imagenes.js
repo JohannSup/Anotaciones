fcf5bbecd4779ca5e08ddfb7e1db871956451d89
HEGA18 <20203tn158@utez.edu.mx>
2022-07-04 12:13:29 -0500

    Practica de imagenes carrucel

--- js/Practica-Imagenes.js
+++ js/Practica-Imagenes.js
@@ -1,45 +1,106 @@
 
-const imagenes = [
-'images/Practica-Imagenes/imagen1.jpg',
-'images/Practica-Imagenes/imagen2.jpg',
-'images/Practica-Imagenes/imagen3.jpg',
-'images/Practica-Imagenes/imagen4.jpg',
-'images/Practica-Imagenes/imagen5.jpg',
-]
 
-
-
-
-function siguiente(){
-    var img = document.getElementById("imagenes");
-    // img.src= imagenes[1]
-    // console.log(index)
-
-    for(var i = 0; i < imgArray.length;i++){
+    window.onload = function () {
         
-            if(i === imgArray.length){
-                document.getElementById("imagenes").src = imgArray[0].src;
-                break;
+        const IMAGENES = [
+                '/images/Practica-Imagenes/imagen1.jpg',
+                '/images/Practica-Imagenes/imagen2.jpg',
+                '/images/Practica-Imagenes/imagen3.jpg',
+                '/images/Practica-Imagenes/imagen4.jpg',
+                '/images/Practica-Imagenes/imagen5.jpg'
+            ];
+
+
+        let posicionActual = 0;
+        let $botonAtras = document.querySelector('#atras');
+        let $botonAdelante = document.querySelector('#adelante');
+        let $imagen = document.querySelector('#imagen');
+
+        
+        function adelantar() {
+            if(posicionActual >= IMAGENES.length - 1) {
+                posicionActual = 0;
+            } else {
+                posicionActual++;
             }
-            document.getElementById("imagenes").src = imgArray[i+1].src;
-            break;
-        
-    }
-}
+            mostrarImagen();
+        }
 
-function atras(){
-    var img = document.getElementById("imagenes");
-    for(var i = imgArray.length-1; i >=0 ;i--){
         
-            if(i === imgArray.length){
-                document.getElementById("imagenes").src = imgArray[4].src;
-                break;
+        function atras() {
+            if(posicionActual <= 0) {
+                posicionActual = IMAGENES.length - 1;
+            } else {
+                posicionActual--;
             }
-            document.getElementById("imagenes").src = imgArray[i-1].src;
-            break;
+            mostrarImagen();
+        }
+
         
-    }
-}
+        function mostrarImagen () {
+            $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+        }
+
+       
+        
+
+        $botonAtras.addEventListener('click', adelantar);
+        $botonAdelante.addEventListener('click', atras);
+
+        mostrarImagen();
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function cambiar(index){
+//     var img = document.getElementById("imagenes");
+//     // img.src= imagenes[1]
+//     // console.log(index)
+
+//     for(var i = 0; i < imgArray.length;i++){
+        
+//             if(i === imgArray.length){
+//                 document.getElementById("imagenes").src = imgArray[0].src;
+//                 break;
+//             }
+//             document.getElementById("imagenes").src = imgArray[i+1].src;
+//             break;
+        
+//     }
+// }
+
+// function cambiar(){
+//     var img = document.getElementById("imagenes");
+//     for(var i = imgArray.length-1; i >=0 ;i--){
+        
+//             if(i === imgArray.length){
+//                 document.getElementById("imagenes").src = imgArray[4].src;
+//                 break;
+//             }
+//             document.getElementById("imagenes").src = imgArray[i-1].src;
+//             break;
+        
+//     }
+// }
 
 
 
